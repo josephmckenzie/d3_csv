0e2e039540255e82856e2a33750e48879b6d33fc
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-12-19 18:14:02 -0500

    add ability to add new station and price to csv file

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'csv'
 
 get '/' do
     erb :index
+end
+
+post '/update' do
+station = params["station"]
+price = params["price"]
+CSV.open("public/gas_prices.csv", "ab") do |csv|
+  csv << [station,price]
+    csv.close
+end
+redirect '/'
 end
